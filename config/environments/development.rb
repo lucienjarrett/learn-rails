712fba307f0b2d26758d2b42b12fd3c6f0ec0a90
Lucien <lucien.jarrett@gmail.com>
2014-04-24 11:43:13 -0500

    modify configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,15 +16,22 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
- config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: ENV["DOMAIN_NAME"]
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      user_name: ENV["GMAIL_PASSWORD"]
 
+
+ config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => ENV["DOMAIN_NAME"]
+      :authentication => "plain",
+      :enable_starttls_auto => true,
+      :user_name => ENV["GMAIL_USERNAME"],
+      :password => ENV["GMAIL_PASSWORD"]
+}
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
   config.action_mailer.perform_deliveries = true
 
 
